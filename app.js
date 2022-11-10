656824c0edbbab649098800893dc2e20eae59997
Name <you@example.com>
2022-11-10 18:03:20 +0200

    add logic working with hash

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
 
-function generateRandColor() {
+
+function generateRandColor(init) {
+
     let hexCodes = '1234567890ABCDEF';
     let color = "#";
+    let colorFromHash = getColorFromHash();
 
-    for (let i = 0; i < 6; i++) {
-        color += hexCodes[Math.round(Math.random() * (hexCodes.length - 1))];
+    if (colorFromHash.length > 1 && isInitial) {
+        isInitial = false;
+        color = colorFromHash;
+    } else {
+        for (let i = 0; i < 6; i++) {
+            color += hexCodes[Math.round(Math.random() * (hexCodes.length - 1))];
+        }
     }
-    
-    return color;
+
+    updateColorHash(color);
+    changeColors(color);
+}
+
+function getColorFromHash() {
+    let hash = document.location.hash;
+    if (hash.length > 1) {
+        return hash;
+    }
+    return '';
+}
+
+function updateColorHash(color = '') {
+    document.location.hash = color.substring(1);
 }
 
 function copyText(text) {
     return navigator.clipboard.writeText(text)
 }
 
-
-const button = document.getElementById('btn');
-
-button.addEventListener('click', () => {
-    let randomColor = generateRandColor();
+function changeColors(randomColor) {
     document.body.style.backgroundColor = randomColor;
     document.querySelector('span').style.color = randomColor;
     document.querySelector('span').innerText = randomColor;
@@ -27,6 +44,15 @@ button.addEventListener('click', () => {
 
     document.querySelector('h2').style.color = luminance > 0.5 ? 'white' : 'black';
     document.querySelector('h2').style.backgroundColor = luminance < 0.5 ? 'white' : 'black';
+}
+
+let isInitial = true;
+
+// action for "ganerate color button"
+const button = document.getElementById('btn');
+button.addEventListener('click', () => {
+    generateRandColor(isInitial);
+    
 });
 
 // copy color code on click
@@ -35,6 +61,8 @@ colorCode.addEventListener('click', () => {
     copyText(colorCode.innerText);
 })
 
-//TODO: add has to link
+
+
 //TODO: change color depend on hash in link
 
+generateRandColor();
